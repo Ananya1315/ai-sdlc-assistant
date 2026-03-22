6315936efc5f0f2e6461b18c60cebe2f25c32d52
Ananya1315 <anusamudrala4@gmail.com>
2026-03-22 11:02:24 +0530

    Version before deployment

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,11 @@ class Project(Base):
     user_stories = Column(Text)
     acceptance_criteria = Column(Text)
     test_cases = Column(Text)
+    user_id = Column(Integer)
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(Text, unique=True, index=True)
+    password = Column(Text)
+    is_pro = Column(Integer, default=0)
